f8f5ccd20c96c570089ac5207df962ad216815b7
youzhen <yl301114@gmail.com>
2023-05-02 12:04:46 +0800

    style, data

--- public/js/calendarSet.js
+++ public/js/calendarSet.js
@@ -1,7 +1,7 @@
 let course = [
     {
         free: 0,
-        time: "0:00",
+        time: "0:00-0501",
         name: "課程名稱",
         teacher: "高偉君",
         date: "2023, 05, 01",
@@ -9,7 +9,7 @@ let course = [
     },
     {
         free: 1,
-        time: "0:00",
+        time: "0:00-0512",
         name: "課程名稱",
         teacher: "高偉君",
         date: "2023, 05, 12",
@@ -17,7 +17,7 @@ let course = [
     },
     {
         free: 1,
-        time: "0:00",
+        time: "0:00-0612",
         name: "課程名稱",
         teacher: "高偉君",
         date: "2023, 06, 12",
@@ -25,10 +25,18 @@ let course = [
     },
     {
         free: 1,
-        time: "0:00",
+        time: "0:00-0712",
         name: "課程名稱",
         teacher: "高偉君",
         date: "2023, 07, 12",
+        target: "這邊放連結",
+    },
+    {
+        free: 1,
+        time: "0:00-0312",
+        name: "課程名稱",
+        teacher: "高偉君",
+        date: "2023, 03, 12",
         target: "這邊放連結",
     }
 ]
@@ -57,44 +65,63 @@ caleandar(element, events, settings);
 //     $(this).text(weekName[i]);
 // })
 
-let todayMonth = $(".cld-datetime").find(".today").attr("data-month");
+let todayMonth;
+let monthData = new Array;
 function set(){
     $(function(){
+        let j=0;
         for(let i=0; i<day.length; i++) {
+            todayMonth = $(".cld-datetime").find(".today").attr("data-month");
             let dayGetMonth = day[i].getMonth() + 1;
+            // console.log("todayMonth=" + todayMonth + ",dayGetMonth=" + dayGetMonth);
             if(dayGetMonth == todayMonth) {
-                let calanderCourse = $("<div class='calander_course'></div>");
-                let badge;
-                if (course[i].free == 0) {
-                    badge = $("<div class='badge badge-blue'>免費</div>")
-                }else {
-                    badge = $("<div class='badge badge-red'>付費</div>")
+                monthData[j] = {
+                    free: course[i].free,
+                    time: course[i].time,
+                    name: course[i].name,
+                    teacher: course[i].teacher,
+                    target: course[i].target,
                 }
-                let link = $("<a class='d-block font-18px'></a>").attr("href",course[i].target);
-                let time = course[i].time;
-                let br = $("<br>");
-                let name = $("<span class='mr-2'></span>").text(course[i].name);
-                let teacher = course[i].teacher;
-                link.append(time,br,name,teacher);
-                calanderCourse.append(badge, link);
-    
-                $(".cld-title").eq(i).append(calanderCourse);
-            }else {
-                // console.log("#########");
+                console.log("monthData[" +j + "]=" + monthData[j].free + "," + monthData[j].time + "," + monthData[j].name + "," + monthData[j].teacher + "," + monthData[j].target);
+                j=j+1;
             }
         }
+        for(let i=0; i<monthData.length; i++){
+            let calanderCourse = $("<div class='calander_course'></div>");
+            let badge;
+            if (monthData[i].free == 0) {
+                badge = $("<div class='badge badge-blue'>免費</div>")
+            }else {
+                badge = $("<div class='badge badge-red'>付費</div>")
+            }
+            let link = $("<a class='d-block font-18px'></a>").attr("href",monthData[i].target);
+            let time = monthData[i].time;
+            let br = $("<br>");
+            let name = $("<span class='mr-2'></span>").text(monthData[i].name);
+            let teacher = monthData[i].teacher;
+            link.append(time,br,name,teacher);
+            calanderCourse.append(badge, link);
+
+            $(".cld-title").eq(i).append(calanderCourse);
+
+        }
+        // console.log("monthData=" + monthData[0].free);
     })
 }
 // set();
 
-$(function(){
-    $(".cld-nav").each(function(){
-        $(this).click(function(){
-            console.log("click!!!")
-            set();
-        })
-    })
+$(document).on("click",".cld-nav",function(){
+    console.log("click!!!")
+    set();
 })
+// $(function(){
+//     $(".cld-nav").each(function(){
+//         $(this).click(function(){
+//             console.log("click!!!")
+//             set();
+//         })
+//     })
+// })
 
   $('#calendarModal').on('shown.bs.modal', function (event) {
     set();
